ac1c2f63010c032850e2c5b2ab9ff3493930eb73
Test User <test@example.com>
2026-07-31 23:59:47 +0100

    fix(dashboard): remove hardcoded secrets and use env variables

--- dashboard/config/config.cjs
+++ dashboard/config/config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-    clientId: process.env.CLIENT_ID || '1271068336188555305',
-    clientSecret: process.env.CLIENT_SECRET || 'bL9fNXSKqMYtxpWPP1Iipig1-IpDcNHt',
-    token: process.env.DISCORD_BOT_TOKEN || "",
-    prefix: "!"
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    token: process.env.DISCORD_BOT_TOKEN,
+    prefix: process.env.PREFIX || "!"
 };
